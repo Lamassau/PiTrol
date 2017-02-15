e5056f3fcee0248ddc69cc2a57cb9acdc1849078
Alhasan A. Al-Nasiry <alhasan.nasiry@gmail.com>
2017-02-15 17:49:03 +0300

    some documentation for es6 spreading

--- src/webapp.js
+++ src/webapp.js
@@ -17,6 +17,7 @@ export default class WebApp extends Component
     return (
       <div className="fluid-container">
         <div className="row">
+          {/*  confused about the `...e.props` ? read about "es6 spread operator" and "react spread attributes"*/}
           { componentList.map((e, i) => <div className="col-md-4"><e.ref {...e.props} /></div>) }
         </div>
       </div>
