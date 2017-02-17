36345cd01a513afab370d9b6c72127548df7206b
Fadi Hanna Al-Kass <fadi.eliya@yahoo.com>
2017-02-16 20:41:58 -0600

    .

--- move-plugins/moveplugins.js
+++ move-plugins/moveplugins.js
@@ -1,8 +1,8 @@
 module.exports.movePlugins = function (src, dest) {
-	var exec = require('child_process').exec
-	exec(`cp -r ${src} ${dest}`, function (error, stdout, stderr) {
-		if (error) {
-			console.error(`exec error: ${error}`)
-		}
-	})
+  var exec = require('child_process').exec
+  exec(`cp -r ${src} ${dest}`, function (error, stdout, stderr) {
+    if (error) {
+      console.error(`exec error: ${error}`)
+    }
+  })
 }
