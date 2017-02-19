05581412b98d5a0c28ebeb11fe60e3310c5f6c73
Fadee Kannah <fadeekannah@gmail.com>
2017-02-19 11:01:54 -0800

    Fix postbuild script deleting plugins folder

--- move-plugins/moveplugins.js
+++ move-plugins/moveplugins.js
@@ -1,8 +1,9 @@
 module.exports.movePlugins = function (src, dest) {
   var exec = require('child_process').exec
-  exec(`cp -r ${src} ${dest}`, function (error, stdout, stderr) {
-    if (error) {
-      console.error(`exec error: ${error}`)
-    }
+  exec(`rm ${dest}${src} -rf`, function (error) {
+    if (error) console.error(`exec error: ${error}`)
+    exec(`cp -r ${src} ${dest}`, function (error, stdout, stderr) {
+      if (error) console.error(`exec error: ${error}`)
+    })
   })
 }
